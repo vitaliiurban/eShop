bc06da31d4c25bc524ce924c441c45666d9f8a96
vitaliiurban <vitalii.urban.ukr@gmail.com>
2023-06-09 21:48:32 +0300

    updated styles on Product page and in Header

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { CartState, addToCart, deleteFromCart } from "../../redux/cartSlice";
 import { RootState } from "../../redux/store";
 import { useGetProductQuery } from "../../redux/productsApi";
-import { useParams } from "react-router-dom";
 import "./_product.scss";
 
 function Product() {
@@ -25,29 +24,10 @@ function Product() {
 
   const [mainImage, setMainImage] = useState<string>(product?.images[0] || "");
   const [primaryImages, setPrimaryImages] = useState<string[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [quantity, setQuantity] = useState<number>(1);
 
-  useEffect(() => {
-    setToggleCart(cartList.some((p) => p.id === product?.id));
-  }, [cartList, product]);
-
-  useEffect(() => {
-    if (product) {
-      setMainImage(product.images[0] || "");
-      setPrimaryImages([product.images[1], product.images[2]]);
-    }
-  }, [product]);
-  useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cartList));
-  }, [cartList]);
-
-  const handleImage = (image: string, number: number) => {
-    const updatedPrimaryImage = [...primaryImages];
-    updatedPrimaryImage[number] = mainImage;
-    setMainImage(image);
-    setPrimaryImages(updatedPrimaryImage);
-  };
-
+  console.log("index", currentIndex);
   const toggleProductCart = (product: ProductModule) => {
     if (!toggleCart) {
       dispatch(addToCart(product));
@@ -58,27 +38,94 @@ function Product() {
     }
   };
 
+  useEffect(() => {
+    setToggleCart(cartList.some((p) => p.id === product?.id));
+  }, [cartList, product]);
+  useEffect(() => {
+    if (product) {
+      setMainImage(product.images[0] || "");
+      setPrimaryImages([
+        product.images[0],
+        product.images[1],
+        product.images[2],
+      ]);
+    }
+  }, [product]);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartList));
+  }, [cartList]);
+
+  useEffect(() => {
+    setMainImage(product?.images[currentIndex] || "");
+  }, [currentIndex]);
+
+  const handleImage = (image: string, index: number) => {
+    setMainImage(image);
+    setCurrentIndex(index);
+  };
+  const handleArrowLeft = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const handleArrowRight = () => {
+    if (currentIndex < 2) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
   return (
     <div className="product-container">
       {product && (
         <div className="breadcrumb">
-          {product.category.charAt(0).toUpperCase() +
-            product.category.slice(1) +
-            ` / ` +
-            product.title}
+          <div className="breadcrumb-category">
+            {product.category.charAt(0).toUpperCase() +
+              product.category.slice(1)}
+          </div>
+          <div>{"/"}</div>
+          <div className="breadcrumb-title">{product.title}</div>
         </div>
       )}
       <div className="product">
         <div className="product-image">
           <div className="product-image-container-main">
-            <img className="product-image-main" src={mainImage}></img>
+            <div
+              onClick={() => handleArrowLeft()}
+              className="product-arrow-container"
+            >
+              <div
+                className={`product-arrow ${
+                  currentIndex > 0 ? "active" : "inactive"
+                }`}
+              >
+                {"<"}
+              </div>
+            </div>
+            <div className="product-image-main-container">
+              <img className="product-image-main" src={mainImage}></img>
+            </div>
+            <div
+              onClick={() => handleArrowRight()}
+              className="product-arrow-container"
+            >
+              <div
+                className={`product-arrow ${
+                  currentIndex < 2 ? "active" : "inactive"
+                }`}
+              >
+                {">"}
+              </div>
+            </div>
           </div>
           <div className="product-image-container-primary-all">
             {primaryImages?.map((primaryImage, index: number) => {
               return (
                 <div
                   onClick={() => handleImage(primaryImage, index)}
-                  className="product-image-container-primary-one"
+                  className={`product-image-container-primary-one ${
+                    primaryImage === mainImage ? "active" : "inactive"
+                  }`}
                   key={index}
                 >
                   <img
@@ -96,6 +143,7 @@ function Product() {
             <div className="product-info-description">
               {product.description}
             </div>
+            <div className="line"></div>
             <div className="product-info-price">
               {product.price * quantity + ` $`}
             </div>
@@ -116,9 +164,37 @@ function Product() {
                 +
               </div>
             </div>
-            <button onClick={() => toggleProductCart(product)}>
-              {toggleCart ? "Delete from Cart" : "Add to Cart"}
-            </button>
+            <div className="line"></div>
+            <div className="product-buttons">
+              <div
+                className="product-button buynow"
+                onClick={() => console.log("buy now")}
+              >
+                {"Buy now"}
+              </div>
+              <div
+                className={`product-button ${toggleCart ? "del" : "add"}`}
+                onClick={() => toggleProductCart(product)}
+              >
+                {toggleCart ? "Delete from Cart" : "Add to Cart"}
+              </div>
+            </div>
+            <div className="delivery">
+              <div className="delivery-container-free">
+                <div className="delivery-free icon">icon</div>
+                <div className="delivery-free">
+                  <div className="delivery-free-title">Free Delivery</div>
+                  <div>Enter your Postal code for Delivery Availability</div>
+                </div>
+              </div>
+              <div className="delivery-container-return">
+                <div className="delivery-return icon">icon</div>
+                <div className="delivery-return">
+                  <div className="delivery-return-title">Return Delivery</div>
+                  <div>Free 30days Delivery Return. Details</div>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </div>
